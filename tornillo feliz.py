d161db22638bbc6bf6ef17f106b72719f6af72b7
JULINHOGT23 <1485840@senati.pe>
2023-03-03 16:37:53 -0500

    ingresamos los botones

--- tornillo feliz.py
+++ tornillo feliz.py
@@ -1,3 +1,13 @@
-  # Lo posicionamos en un label
+# Lo posicionamos en un label
     label1 =Label(root, text="", font=('courier', 14, 'bold'), bg='white', justify=LEFT)
     label1.grid(row=1,column=1)
+    
+# ponemos los botones dendentro
+    frame=Frame(root,bd=5)
+   
+    
+    anterior = Button(frame,text="Anterior", command=mesAnterior)
+    anterior.grid(row=1, column=1, sticky=W)
+    siguiente =Button(frame,text="Siguiente", command=mesSiguiente)
+    siguiente.grid(row=1, column=2)
+    frame.grid(row=2,column=1)
