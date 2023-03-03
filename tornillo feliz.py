b622498201e764f71e98ba80ad0a884f715e2311
JULINHOGT23 <1485840@senati.pe>
2023-03-03 16:52:20 -0500

    continuamos

--- tornillo feliz.py
+++ tornillo feliz.py
@@ -1,3 +1,6 @@
+from tkinter import *
+from tkinter import messagebox as MessageBox
+
 # Lo posicionamos en un label
     label1 =Label(root, text="", font=('courier', 14, 'bold'), bg='white', justify=LEFT)
     label1.grid(row=1,column=1)
@@ -17,3 +20,9 @@ root.title('Ferretería El tornillo feliz') # nombre del titulo de mi ventana
     root.config(bg= "#BDDCE1") # background o fondo de mi ventana
 #root.iconbitmap('imagen.ico')# imagen con el icono de mi ventana
     root.resizable(False,False) #Evita que se expanda mi ventana
+
+    miFrame.pack() #empaquetando
+    miFrame.config(bg = "#A4D8D4")#fondo de mi primer frame
+    miFrame.config(cursor = "pirate") #miniatura del raton (pirata)
+
+    
